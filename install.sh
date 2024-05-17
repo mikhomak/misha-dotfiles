758417fc453253486464a0bcae36c3c1d1ff98a5
mishanya <homakhaker@gmail.com>
2024-05-17 16:05:14 +0200

    Changed install sh a bit

--- install.sh
+++ install.sh
@@ -3,18 +3,32 @@
 echo "installing symlinks"
 echo "Current directory - ${PWD}"
 BASEDIR=${PWD}
+echo "Starting installation...."
+echo "-------"
+
 
 echo "Installing vim..."
 ln -s -f $BASEDIR/.vimrc $HOME/.vimrc
-
+(test -f $HOME/.vimrc && echo "Vimrc is installed ✅") || echo "Vimrc is not installed!❌"
+echo "-------"
 
 echo "Installing zsh..."
 ln -s -f $BASEDIR/.zshrc $HOME/.zshrc
 echo "export MISHA=$BASEDIR" > $HOME/.zshenv
-(ls $HOME/.zshrc && echo "Zshrc is installed") || echo "Zshrc is not installed!"
+(test -f $HOME/.zshrc && echo "Zshrc is installed ✅") || echo "zshrc is not installed!❌"
+echo "-------"
+
+
 
 echo "Installing p10k..."
 ln -s -f $BASEDIR/.p10k.zsh $HOME/.p10k.zsh
+(test -f $HOME/.p10k.zsh && echo "P10K is installed ✅") || echo "P10K is not installed!❌"
+echo "-------"
+
 
 echo "Installing kitty..."
 ln -s -f $BASEDIR/kitty.conf $HOME/.config/kitty/kitty.conf
+(test -f $HOME/.config/kitty/kitty.conf && echo "Kitty is installed ✅") || echo "Kitty is not installed!❌"
+echo "-------"
+
+
