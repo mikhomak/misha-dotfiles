ff17234390ba8b3831a937cf4820dc9a4944085e
mishanya <homakhaker@gmail.com>
2024-05-21 23:16:01 +0200

    Changed split pane keys

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,15 +12,38 @@ config.window_decorations = "NONE"
 config.keys = {
   -- This will create a new split and run your default program inside it
   {
-    key = 'k',
+    key = 'w',
     mods = 'SHIFT|SUPER',
-	action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	action = wezterm.action.SplitPane{
+		direction = 'Up',
+		size = { Percent = 50},
+},
   },
   {
-    key = 'j',
+    key = 's',
     mods = 'SHIFT|SUPER',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	action = wezterm.action.SplitPane{
+		direction = 'Down',
+		size = { Percent = 50},
+},
   },
+	{
+    key = 'a',
+    mods = 'SHIFT|SUPER',
+	action = wezterm.action.SplitPane{
+		direction = 'Left',
+		size = { Percent = 50},
+},
+  },
+{
+    key = 'd',
+    mods = 'SHIFT|SUPER',
+	action = wezterm.action.SplitPane{
+		direction = 'Right',
+		size = { Percent = 50},
+},
+  },
+
 	{
 	key = 'UpArrow',
     mods = 'SHIFT|SUPER',
