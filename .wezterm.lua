6cf5ea949811e8191ed6dece97000d7b09c14703
mishanya <homakhaker@gmail.com>
2024-05-21 10:28:32 +0200

    Changing a bit the wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,10 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
+config.font = wezterm.font '3270 Nerd Font'
+config.font_size = 16.0
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'catppuccin-latte'
 
 -- and finally, return the configuration to wezterm
 return config
