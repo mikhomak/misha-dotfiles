93e59e9e44cb12601dc5873a06b88ae6b6b26da9
mishanya <homakhaker@gmail.com>
2024-05-21 23:03:36 +0200

    Added neofetch

--- sketchybar/plugins/custom_selected.sh
+++ sketchybar/plugins/custom_selected.sh
@@ -6,10 +6,12 @@ if [ $SELECTED = true ]; then
 	sketchybar --set "$NAME" background.color=$WIN98_BRIGHT_GREY \
 		background.border_color=$BLACK \
 		background.border_width=1 \
-		background.shadow.angle=30
+		background.shadow.angle=30 \
+		icon.color=$WIN98_BLUE
 else
 	sketchybar --set "$NAME" background.color=$WIN98_GREY \
 		background.border_color=$BLACK \
 		background.border_width=1 \
-		background.shadow.angle=220
+		background.shadow.angle=220 \
+		icon.color=$BLACK
 fi
