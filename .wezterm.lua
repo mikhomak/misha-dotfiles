935a357c09dde8f95746aef379bf9bfb77c4a175
mishanya <homakhaker@gmail.com>
2024-05-27 11:24:05 +0200

    Format wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,97 +1,97 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
+	-- This will hold the configuration.
 local config = wezterm.config_builder()
 
-local home_dir = os.getenv("HOME")
+	local home_dir = os.getenv("HOME")
 
-config.font = wezterm.font '3270 Nerd Font'
-config.font_size = 16.0
--- For example, changing the color scheme:
--- config.color_scheme = 'primary'
-config.color_scheme = 'DanQing (base16)'
-config.window_decorations = "NONE"
-config.hide_tab_bar_if_only_one_tab = true
+	config.font = wezterm.font '3270 Nerd Font'
+	config.font_size = 16.0
+	-- For example, changing the color scheme:
+	-- config.color_scheme = 'primary'
+	config.color_scheme = 'DanQing (base16)'
+	config.window_decorations = "RESIZE"
+	config.hide_tab_bar_if_only_one_tab = true
 
-config.inactive_pane_hsb = {
-  saturation = 0.1,
-  brightness = 0.2,
-}
+	config.inactive_pane_hsb = {
+		saturation = 0.1,
+		brightness = 0.2,
+	}
 
 config.background = {
-  {
-    source = {
-      File = home_dir .. '/.config/imgs/eva_2.gif',
-    },
-width = '100%',
-height='Contain',
-    repeat_x = 'Mirror',
-    hsb = {
-	brightness = 0.1,
-},
-    attachment = { Parallax = 0.1 },
-  },
+	{
+		source = {
+			File = home_dir .. '/.config/imgs/eva_2.gif',
+		},
+		width = '100%',
+		height='Contain',
+		repeat_x = 'Mirror',
+		hsb = {
+			brightness = 0.1,
+		},
+		attachment = { Parallax = 0.1 },
+	},
 }
 config.keys = {
-  -- This will create a new split and run your default program inside it
-  {
-    key = 'w',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.SplitPane{
-		direction = 'Up',
-		size = { Percent = 50},
-},
-  },
-  {
-    key = 's',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.SplitPane{
-		direction = 'Down',
-		size = { Percent = 50},
-},
-  },
+	-- This will create a new split and run your default program inside it
 	{
-    key = 'a',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.SplitPane{
-		direction = 'Left',
-		size = { Percent = 50},
-},
-  },
-{
-    key = 'd',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.SplitPane{
-		direction = 'Right',
-		size = { Percent = 50},
-},
-  },
+		key = 'w',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.SplitPane{
+			direction = 'Up',
+			size = { Percent = 50},
+		},
+	},
+	{
+		key = 's',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.SplitPane{
+			direction = 'Down',
+			size = { Percent = 50},
+		},
+	},
+	{
+		key = 'a',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.SplitPane{
+			direction = 'Left',
+			size = { Percent = 50},
+		},
+	},
+	{
+		key = 'd',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.SplitPane{
+			direction = 'Right',
+			size = { Percent = 50},
+		},
+	},
 
 	{
-	key = 'UpArrow',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.ActivatePaneDirection 'Up',
+		key = 'UpArrow',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Up',
 	},
 	{
-	key = 'DownArrow',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.ActivatePaneDirection 'Down',
+		key = 'DownArrow',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Down',
 	},
 	{
-	key = 'LeftArrow',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.ActivatePaneDirection 'Left',
+		key = 'LeftArrow',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Left',
 	},
 	{
-	key = 'RightArrow',
-    mods = 'SHIFT|SUPER',
-	action = wezterm.action.ActivatePaneDirection 'Right',
+		key = 'RightArrow',
+		mods = 'SHIFT|SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Right',
 	},
 	{
-	key = 'w',
-    mods = 'CMD',
-action = wezterm.action.CloseCurrentPane { confirm = true },
+		key = 'w',
+		mods = 'CMD',
+		action = wezterm.action.CloseCurrentPane { confirm = true },
 	},
 
 
