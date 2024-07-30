edfc1f43037163ee77da9f38691950c367de8941
mishanya <homakhaker@gmail.com>
2024-07-30 20:02:51 +0200

    Increased the font size for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 	local home_dir = os.getenv("HOME")
 
 	config.font = wezterm.font '3270 Nerd Font'
-	config.font_size = 11.0
+	config.font_size = 16.0
 	-- For example, changing the color scheme:
 	-- config.color_scheme = 'primary'
 config.color_scheme = 'Rasi (terminal.sexy)'
