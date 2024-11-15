e759e74227bf4e6776b5681d9e263019e4f5ef6d
mishanya <homakhaker@gmail.com>
2024-11-15 10:43:58 +0100

    Added cava to nix

--- linux/nix/home.nix
+++ linux/nix/home.nix
@@ -3,27 +3,31 @@
 {
 
 
-  home.username = "misha";
-  home.homeDirectory = "/home/misha";
+	home.username = "misha";
+	home.homeDirectory = "/home/misha";
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+	home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  home.packages = [
-	  pkgs.jdk17
-	  pkgs.gradle
-	  pkgs.maven
-	  pkgs.gettext
-	  pkgs.procps
-  ];
-  home.file = {
-  };
+		home.packages = [
+# fun
+		pkgs.cava
 
-  home.sessionVariables = {
-	XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME   = "$HOME/var/lib";
-    XDG_CACHE_HOME  = "$HOME/var/cache";
-    # EDITOR = "emacs";
-  };
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+# werk related
+			pkgs.jdk17
+			pkgs.gradle
+			pkgs.maven
+			pkgs.gettext
+			pkgs.procps
+		];
+	home.file = {
+	};
+
+	home.sessionVariables = {
+		XDG_CONFIG_HOME = "$HOME/.config";
+		XDG_DATA_HOME   = "$HOME/var/lib";
+		XDG_CACHE_HOME  = "$HOME/var/cache";
+	    EDITOR = "vim";
+	};
+# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
