ec14cc3bb180409b009bead2e0164c0907621a12
mishanya <homakhaker@gmail.com>
2025-01-03 19:45:19 +0100

    Added gc to nix

--- linux/nix/configuration.nix
+++ linux/nix/configuration.nix
@@ -59,6 +59,12 @@
 	nixpkgs.config.allowUnfree = true;
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+	nix.gc = {
+		automatic = true;
+		dates = "weekly";
+		options = "--delete-older-than 30d";
+	};
+
 # Docker
 	virtualisation.docker.enable = true;
 	virtualisation.docker.rootless = {
